8a898a2e922974c84d1bd7e40c133157001784cf
Legionite <46542779+LegioniteTerraTech@users.noreply.github.com>
2021-06-10 21:41:57 -1000

    Release

---
+++ WhisperProps/Shhhhhh.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Harmony;
+
+namespace WhisperProps
+{
+    public class Shhhhhh
+    {
+        public static void Silence()
+        {
+            //Initiate the madness
+            HarmonyInstance harmonyInstance = HarmonyInstance.Create("legionite.whisperprops.core");
+            harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(ModuleBooster))]
+        [HarmonyPatch("OnPool")]
+        private class ShhhhhhushMyChild
+        {
+            private static void Postfix(ModuleBooster __instance)
+            {
+                FieldInfo lockGet = typeof(ModuleBooster).GetField("m_BoosterAudioType", BindingFlags.NonPublic | BindingFlags.Instance);
+                int m_BoosterAudioType = (int)lockGet.GetValue(__instance);
+                if (m_BoosterAudioType == (int)TechAudio.BoosterEngineType.Propeller)
+                {
+                    lockGet.SetValue(__instance, (int)TechAudio.BoosterEngineType.MediumRotor);
+                }
+            }
+        }
+    }
+}
